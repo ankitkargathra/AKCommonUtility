c491ec58c3f96b8f9d4d3c9b617eea281621d4cc
Ankit <ankit.kargathra@gmail.com>
2019-03-18 18:58:46 +0530

    Add methods for trim and validate url string

--- AKCommonUtility.podspec
+++ AKCommonUtility.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AKCommonUtility'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'AKCommonUtility provides common methods.'
   s.swift_version    = '4.2'
 
--- AKCommonUtility/Classes/AKCommonUtility.swift
+++ AKCommonUtility/Classes/AKCommonUtility.swift
@@ -3,3 +3,22 @@
 
 import UIKit
 import Foundation
+
+public extension String {
+    
+    //Remove white space character and new line character.
+    public func trim() -> String
+    {
+        var str = self.trimmingCharacters(in: NSCharacterSet.whitespaces)
+        str = str.trimmingCharacters(in: NSCharacterSet.newlines)
+        return str
+    }
+    
+    //Validation of url
+    func validateUrl() -> Bool {
+        if let url  = NSURL(string: self) {
+            return UIApplication.shared.canOpenURL(url as URL)
+        }
+        return false
+    }
+}
